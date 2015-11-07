6b31d64d9a638f5e026dd8cd0f97fa767195b2a9
Sylvia Lee <helloimsylvia@gmail.com>
2015-11-06 21:06:23 -0500

    changed project page

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
-$(".row").mouseenter(function(){
-  $(".work-img", this).show( "fast", function() {
-  });
-});
-
-$(".row").mouseenter(function(){
-  $(".work-description", this).show( "fast", function() {
-  });
-});
-
-$(".row").mouseenter(function(){
-  $(".work-description", this).animate({
-    opacity: 1,
-  }, 100);
-});
-
-$(".row").mouseenter(function(){
-  $(".work-img", this).animate({
-    right: "-10px",
-    opacity: 1,
-  }, 1000);
-});
+// $(".row").mouseenter(function(){
+//   $(".work-img", this).show( "fast", function() {
+//   });
+// });
+//
+// $(".row").mouseenter(function(){
+//   $(".work-description", this).show( "fast", function() {
+//   });
+// });
+//
+// $(".row").mouseenter(function(){
+//   $(".work-description", this).animate({
+//     opacity: 1,
+//   }, 100);
+// });
+//
+// $(".row").mouseenter(function(){
+//   $(".work-img", this).animate({
+//     right: "-10px",
+//     opacity: 1,
+//   }, 1000);
+// });
 
 // $('img').load(function() {
 //     $(this).data('height', this.height);
